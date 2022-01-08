27d1d5aa22381803190d7117fa4acf5d1410de64
Ty Jenkins <cientifik85@gmail.com>
2022-01-08 15:21:47 -0500

    initialized body-parser middleware, created validation checks for user registration, defined route for user registration

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+// Initialize Middleware
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

eae26452009574f820a243ddf126099f4689243d
Ty Jenkins <cientifik85@gmail.com>
2022-01-14 22:16:46 -0500

    created two routes: 1. to get all profiles 2. to get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -73,7 +73,6 @@ router.post(
 			profileFields.skills = req.body.skills
 				.split(",")
 				.map((skill) => skill.trim());
-			console.log(profileFields.skills);
 		}
 
 		standardFields.forEach((field) => {
@@ -112,5 +111,45 @@ router.post(
 		}
 	}
 );
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+
+router.get("/", async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+		res.json(profiles);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Server error.");
+	}
+});
+
+// @route   GET api/profile/user/:userid
+// @desc    Get profile by user ID
+// @access  Public
+
+router.get("/user/:user_id", async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate("user", ["name", "avatar"]);
+
+		if (!profile) {
+			return res.status(400).json({ msg: "Profile not found." });
+		}
+
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+
+		if (err.kind == "ObjectId") {
+			return res.status(400).json({ msg: "Profile not found." });
+		}
+
+		res.status(500).send("Server error.");
+	}
+});
 
 module.exports = router;
